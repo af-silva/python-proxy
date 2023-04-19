61bf833543577c270398b655d984ae1dab764a8a
afsilva <andre.silva@petrotec.com>
2023-04-19 12:32:18 +0100

    update to env variables instead of a config file
    add logger

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-import json
+import os
 import socket
+import logging
+import sys
 
-with open('/app/config.json') as f:
-    config = json.load(f)
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-bind_ip = config['bind_ip']
-bind_port = config['bind_port']
-dest_ip = config['dest_ip']
-dest_port = config['dest_port']
+bind_ip = os.environ['BIND_IP']
+bind_port = int(os.environ['BIND_PORT'])
+dest_ip = os.environ['DEST_IP']
+dest_port = int(os.environ['DEST_PORT'])
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
 server.listen(5)
 
-print(f'[*] Listening on {bind_ip}:{bind_port}')
+logger.info(f'[*] Listening on {bind_ip}:{bind_port}')
 
 while True:
     client_socket, addr = server.accept()
 
-    print(f'[*] Accepted connection from {addr[0]}:{addr[1]}')
+    logger.info(f'[*] Accepted connection from {addr[0]}:{addr[1]}')
 
     destination_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     destination_socket.connect((dest_ip, dest_port))
@@ -28,7 +35,7 @@ while True:
         if not data:
             break
         destination_socket.send(data)
-        print(f'[+] Sent {len(data)} bytes to {dest_ip}:{dest_port}')
+        logger.info(f'[+] Sent {len(data)} bytes to {dest_ip}:{dest_port}')
 
     destination_socket.close()
     client_socket.close()
